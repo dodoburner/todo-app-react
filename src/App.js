bdebf65624c81f25e6267c06684666080348589e
dodoburner <100496179+dodoburner@users.noreply.github.com>
2023-02-11 13:11:13 +0100

    Use localStorage to save tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import TaskForm from "./components/TaskForm";
 import Tasks from "./components/Tasks";
 import "./style/App.css";
@@ -6,29 +6,7 @@ import "./style/App.css";
 export const taskContext = createContext(null);
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      title: "volim taskove",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
-      id: 1,
-      completed: false,
-    },
-    {
-      title: "jooj meni",
-      description:
-        "when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
-      id: 2,
-      completed: false,
-    },
-    {
-      title: "aaaaaa",
-      description:
-        "it to make a type specimen book. It has survived not only five centuries",
-      id: 3,
-      completed: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const setCompleted = (id) => {
     setTasks((prevTasks) => {
@@ -48,6 +26,15 @@ function App() {
   const deleteTask = (id) => {
     setTasks((prevTasks) => [...prevTasks.filter((el) => el.id !== id)]);
   };
+
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    setTasks(savedTasks);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <div className="App">
